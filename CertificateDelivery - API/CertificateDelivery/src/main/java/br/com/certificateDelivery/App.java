1fe87b90e0fe5885ecea0ee759f3af943d0f2bab
Layner <layner2008@hotmail.com>
2017-02-24 17:56:17 -0300

    'Commit da classe de execução da aplicação'

---
+++ CertificateDelivery - API/CertificateDelivery/src/main/java/br/com/certificateDelivery/App.java
@@ -0,0 +1,15 @@
+package br.com.certificateDelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import br.com.certificateDelivery.utils.AppContext;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args){
+		SpringApplication.run(AppContext.class, args);
+	}
+	
+}
---
+++ CertificateDelivery - API/CertificateDelivery/src/main/java/br/com/certificateDelivery/utils/AppContext.java
@@ -0,0 +1,12 @@
+package br.com.certificateDelivery.utils;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan(basePackages = {"br.com.certificateDelivery"})
+public class AppContext {
+
+}
