ebced773e3ea1b1bc2267ff00c5a2504e766eeea
Layner <layner2008@hotmail.com>
2017-03-02 20:07:08 -0300

    Commit dos artefatos de usuario

---
+++ CertificateDelivery - Web/app/src/app.module.js
@@ -0,0 +1,9 @@
+angular.module('app', ['ngRoute'])
+    .config(function($routeProvider){
+       $routeProvider.
+            when('/cadUsuario', {
+                templateUrl: 'src/usuario/usuario.html',
+                controller: 'UsuarioCtrl'
+            })
+       
+    });
